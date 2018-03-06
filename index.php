64c5f42021a5d5b09fad9bcef151b571aed1c0b6
robert <robertvandenbaar@gmail.com>
2018-03-06 21:19:51 +0100

    Removed display of errors

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 define('BASE_DIR', dirname(__FILE__));
 define('BASE_URL', str_replace($_SERVER['DOCUMENT_ROOT'], '', BASE_DIR));
 
