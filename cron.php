acdf1e664af116b8e5b9f0db8879e35beda96533
robert <robertvandenbaar@gmail.com>
2021-07-19 17:39:46 +0200

    Show date time (for debugging purposes) Reformatted code

--- cron.php
+++ cron.php
@@ -9,13 +9,13 @@ define('BASE_DIR', dirname(__FILE__));
 
 require 'app/vendor/autoload.php';
 
-if (php_sapi_name() != 'cli')
-{
-	die('This file should be run on the commandline');
+if (php_sapi_name() != 'cli') {
+    die('This file should be run on the commandline');
 }
 
 echo "\n\nStart\n";
 echo "--------------------------\n";
+echo date('Y-m-d H:i:s') . "\n";
 
 // reset search index
 $search = new Search();
@@ -23,40 +23,35 @@ $search->resetIndex();
 
 function updateRecursively($baseDir, $search)
 {
-	foreach (scandir($baseDir) as $file)
-	{
-		if(substr($file, 0, 1) == '.')
-		{
-			continue;
-		}
+    foreach (scandir($baseDir) as $file) {
+        if (substr($file, 0, 1) == '.') {
+            continue;
+        }
 
-		$fullPath = $baseDir . '/' . $file;
+        $fullPath = $baseDir . '/' . $file;
 
-		if (is_dir($fullPath))
-		{
-			updateRecursively($fullPath, $search);
-			continue;
-		}
+        if (is_dir($fullPath)) {
+            updateRecursively($fullPath, $search);
+            continue;
+        }
 
-		$file = new File($fullPath);
+        $file = new File($fullPath);
 
-		if (in_array($file->getExtension(), Setting::get('supported_extensions')))
-		{
-			$file = new Image($fullPath);
+        if (in_array($file->getExtension(), Setting::get('supported_extensions'))) {
+            $file = new Image($fullPath);
 
-			echo "Updating thumbnail and mid-size image for " . $fullPath . "\n";
+            echo "Updating thumbnail and mid-size image for " . $fullPath . "\n";
 
-			// to speed up the cron process the detail images are not generated if the default option is full size
-			if (!Setting::get('full_size_by_default'))
-			{
-				$file->updateDetail();
-			}
-			$file->updateThumbnail();
+            // to speed up the cron process the detail images are not generated if the default option is full size
+            if (!Setting::get('full_size_by_default')) {
+                $file->updateDetail();
+            }
+            $file->updateThumbnail();
 
-		}
+        }
 
-		$file->updateIndex($search);
-	}
+        $file->updateIndex($search);
+    }
 }
 
 updateRecursively(Setting::get('image_base_dir'), $search);
